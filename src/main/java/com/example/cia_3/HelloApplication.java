e74c630e84c57112523123134b10c2d642260e98
yashlucas <yash.lucas@science.christuniversity.in>
2022-05-24 09:33:22 +0530

    Updates

--- src/main/java/com/example/cia_3/HelloApplication.java
+++ src/main/java/com/example/cia_3/HelloApplication.java
@@ -10,15 +10,17 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hii!");
         stage.setScene(scene);
         stage.show();
         // comment by ySH
     }
 
     public static void main(String[] args) {
+        System.out.println("My name is Yash Lucas");
+        System.out.println("My name is  Lucas");
         launch();
     }
 }
--- src/main/java/com/example/cia_3/HelloController.java
+++ src/main/java/com/example/cia_3/HelloController.java
@@ -1,14 +1,24 @@
 package com.example.cia_3;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onHelloButtonClick() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Railway  System");
+        stage.setScene(scene);
+        stage.show();
     }
 }
