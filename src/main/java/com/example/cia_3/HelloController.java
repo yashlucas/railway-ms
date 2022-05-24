dca8ff3a3ce0a2f006b099d9b0abf77b3526d64e
yashlucas <yash.lucas@science.christuniversity.in>
2022-05-24 19:59:15 +0530

    Updates

--- src/main/java/com/example/cia_3/HelloController.java
+++ src/main/java/com/example/cia_3/HelloController.java
@@ -13,7 +13,8 @@ public class HelloController {
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() throws IOException {
+    protected void onHelloButtonClick() throws IOException
+    {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         Stage stage = new Stage();
@@ -21,4 +22,15 @@ public class HelloController {
         stage.setScene(scene);
         stage.show();
     }
+    @FXML
+    protected void onHelloButtonClick_1() throws IOException
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Stage stage = new Stage();
+        stage.setTitle("Railway  System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
